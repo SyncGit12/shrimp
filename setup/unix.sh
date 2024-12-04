c174f790c02b0298b9e12b6e3841b57c8810fa88
Lunar <144155299+SyncGit12@users.noreply.github.com>
2024-12-04 13:45:59 -0300

    fixed compiling, workflows done

--- setup/unix.sh
+++ setup/unix.sh
@@ -1,6 +1,9 @@
-@echo off
-title Installing bullshit
-echo this might take a lil while :DDDD
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
 haxelib setup
 haxelib install discord_rpc 1.0.0
 haxelib install flixel-addons 2.11.0
@@ -13,9 +16,6 @@ haxelib install openfl 9.3.0
 haxelib install lime
 haxelib run lime setup flixel
 haxelib run lime setup
-clear
-
-title Setting bullshit
 haxelib set discord_rpc 1.0.0
 haxelib set flixel-addons 2.11.0
 haxelib set flixel-demos 2.9.0
@@ -25,5 +25,4 @@ haxelib set flxanimate 3.0.4
 haxelib git hxCodec https://github.com/SyncGit12/hxCodec
 haxelib set openfl 9.3.0
 haxelib set lime
-echo fucking done folks
-exit
+echo Finished!
